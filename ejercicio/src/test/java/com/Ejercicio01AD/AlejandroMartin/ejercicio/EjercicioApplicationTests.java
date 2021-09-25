c473e3275d316e81b5230bd4a36b0158bc62b33f
AleMarCue <74199687+alejandrosalesianos@users.noreply.github.com>
2021-09-25 18:27:52 +0200

    Subido ejercicio

---
+++ ejercicio/src/test/java/com/Ejercicio01AD/AlejandroMartin/ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ejercicio01AD.AlejandroMartin.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
